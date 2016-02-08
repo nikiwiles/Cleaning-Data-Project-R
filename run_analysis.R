86eedd0765aaefd5ff67b281c85a2329f7fd0ca7
Niki Wiles <niki.wiles@360i.com>
2016-02-08 11:30:21 +0000

    Tidy up comments

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@ library(dplyr)
 
 # The project expects the contents of this zip file ...
 # https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
-# .. to be extracted at this location in the working directory.
+# .. to be extracted at this location relative to the working directory.
 dataDirectory <- "./Data/"
 
 # Function for reading in our data files
@@ -10,7 +10,7 @@ readData <- function (directory, folder, filename) {
   read.table(paste0(directory, folder, "/", filename), sep = "")
 }
 
-# Load X_train and X_test data sets
+# Load X_train.txt and X_test.txt data sets
 train <- readData (dataDirectory, "train", "X_train.txt")
 test <- readData (dataDirectory, "test", "X_test.txt")
 
@@ -18,40 +18,39 @@ test <- readData (dataDirectory, "test", "X_test.txt")
 train_activity <- readData (dataDirectory, "train", "y_train.txt")
 test_activity <- readData (dataDirectory, "test", "y_test.txt")
 
-# Load subject train and test data
+# Load subject "train" and "test" data (which splits the data up by participant)
 train_subjects <- readData (dataDirectory, "train", "subject_train.txt")
 test_subjects <- readData (dataDirectory, "test", "subject_test.txt")
 
-# Merge activity and subject data 
+# Merge activity and subject data for "test" and "train" 
 test <- cbind(test, test_subjects, test_activity)
 train <- cbind(train, train_subjects, train_activity)
 
-# Merge the two data sets
+# Merge the "test" and "train" data sets
 allData <- rbind(train, test)
 
-# Read in our features
+# Read in our features list
 features <- readData (dataDirectory, "", "features.txt")
 
-# Tack on the columns that we just merged in
+# Tack on the names of the two columns that we created
 allFeatures <- c(sapply(features$V2, function(x) {as.character(x)}), "subject", "activityid")
 
 # Get indexes mentioning the columns we'd like to keep
 colsToSelect <- grep("(M|m)ean|(S|s)td|subject|activityid", allFeatures)
 
-# Select only those columns
+# Subset only those columns
 allData <- allData[ ,colsToSelect]
 
 # Name all of the columns
 names(allData) <- grep("(M|m)ean|(S|s)td|subject|activityid", allFeatures, value = TRUE)
 
-# Read in the activity labels
+# Read in the activity labels, and rename the columns to make it easy to join the data
 activitylabels <- readData (dataDirectory, "", "activity_labels.txt") %>% rename(activityid = V1, activityname = V2)
 
-# Merge
+# Merge, and select everything except the activityid
 allData <- allData %>% merge(activitylabels) %>% select(-activityid)
 
-# Clean up our column names, and make them a little more descriptive
-# I'd also note that the column names were probaly more or less fine as they were.
+# Fn to clean up our column names, and make them a little more descriptive
 cleanColumnNames <- function (x){
   x <- tolower(x)
   x <- gsub("\\()", "", x)
@@ -68,14 +67,14 @@ cleanColumnNames <- function (x){
   x
 }
 
-# Clean up names
+# Clean up names by applying the function to names(allData)
 names(allData) <- sapply(names(allData), cleanColumnNames)
 
 # Get the mean of all columns, broken down by activity and subject
 summaryMeans <- allData %>% group_by(activityname, subject) %>% summarise_each(funs(mean))
 
-# Save the output (trying to match the structure of the source data)
+# Save the output
 write.table(summaryMeans, file='summaryMeans.tsv', quote=FALSE, sep=' ', col.names=FALSE)
 
-# Output column names
+# Save column names
 write.table(names(summaryMeans), file='features.tsv', quote=FALSE, sep=' ', col.names=FALSE)
